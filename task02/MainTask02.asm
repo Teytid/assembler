33d0f6116125c2c6993b1e71ad1370ee6712a009
Teytid <62693615+Teytid@users.noreply.github.com>
2020-10-10 15:13:57 +0000

    Update MainTask02.asm

--- task02/MainTask02.asm
+++ task02/MainTask02.asm
@@ -8,164 +8,170 @@ entry start
 
 include 'win32a.inc'
 
-;--------------------------------------------------------------------------
+;------------------------------------------------------------------------—
 section '.data' data readable writable
 
-        strVecSize   db 'size of vector? ', 0
-        strIncorSize db 'Incorrect size of vector = %d', 10, 0
-        strVecElemI  db '[%d]? ', 0
-        strScanInt   db '%d', 0
-        strFirstNeg    db 'first negatuve value %d', 10, 0
-        strVecElemOut  db '[%d] = %d', 10, 0
+strVecSize db 'size of vector? ', 0
+strIncorSize db 'Incorrect size of vector = %d', 10, 0
+strVecElemI db '[%d]? ', 0
+strScanInt db '%d', 0
+strFirstNeg db 'first negatuve value %d', 10, 0
+strVecElemOut db '[%d] = %d', 10, 0
 
-        vec_size     dd 0
-        sum          dd 0
-        help         dd ?
-        negv         dd 0
-        i            dd ?
-        tmp          dd ?
-        tmpStack     dd ?
-        vec          rd 1024
-        vec2         rd 1024
+vec_size dd 0
+sum dd 0
+help dd ?
+negv dd 0
+i dd ?
+tmp dd ?
+tmp2 dd ?
+tmpStack dd ?
+vec rd 1024
+vec2 rd 1024
 
-;--------------------------------------------------------------------------
+;------------------------------------------------------------------------—
 section '.code' code readable executable
 start:
 ; 1) vector input
-        call VectorInput
+call VectorInput
 ; 2) test vector out
-        call VectorOut
+call VectorOut
 finish:
-        call [getch]
+call [getch]
 
-        push 0
-        call [ExitProcess]
+push 0
+call [ExitProcess]
 
-;--------------------------------------------------------------------------
+;------------------------------------------------------------------------—
 VectorInput:
-        push strVecSize
-        call [printf]
-        add esp, 4
+push strVecSize
+call [printf]
+add esp, 4
 
-        push vec_size
-        push strScanInt
-        call [scanf]
-        add esp, 8
+push vec_size
+push strScanInt
+call [scanf]
+add esp, 8
 
-        mov eax, [vec_size]
-        cmp eax, 0
-        jle  failedSize
-        cmp eax, 1024
-        jg failedSize
+mov eax, [vec_size]
+cmp eax, 0
+jle failedSize
+cmp eax, 1024
+jg failedSize
 ; else continue...
 getVector:
-        xor ecx, ecx            ; ecx = 0
-        mov ebx, vec
-	mov edx, vec2
+xor ecx, ecx ; ecx = 0
+mov ebx, vec
+mov edx, vec2
 
 
 getVecLoop:
-        mov [tmp], ebx
-        cmp ecx, [vec_size]
-        jge endInputVector       ; to end of loop
+mov [tmp], ebx
+mov [tmp2], edx
+mov [i], ecx
+cmp ecx, [vec_size]
+jge endInputVector ; to end of loop
 
-        ; input element
-        mov [i], ecx
-        push ecx
-        push strVecElemI
-        call [printf]
-        add esp, 8
+; input element
+mov ecx, [i]
+push ecx
+push strVecElemI
+call [printf]
+add esp, 8
 
-        push ebx
-        push strScanInt
-        call [scanf]
-        add esp, 8
+mov edx, [tmp2]
+push edx
+push strScanInt
+call [scanf]
+add esp, 8
 
-        mov edi, [ebx]
-        cmp edi, 0
-        call newNeg
+mov ebx, [tmp]
+mov edi, [ebx]
+cmp edi, 0
+call newNeg
 
-	mov eli, [ebx]
-	cmp eli, 0
-	call comparer
+mov edi, [ebx]
+cmp edi, 0
+call comparer
 
-        mov ecx, [i]
-        inc ecx
-        mov ebx, [tmp]
-        add ebx, 4
-	add edx, 4
-        jmp getVecLoop
+mov ecx, [i]
+inc ecx
+mov ebx, [tmp]
+mov edx, [tmp2]
+add ebx, 4
+add edx, 4
+jmp getVecLoop
 endInputVector:
-        call [printf]
-        add esp, 8
-        ret
+call [printf]
+add esp, 8
+ret
 
-;--------------------------------------------------------------------------
+;------------------------------------------------------------------------—
 VectorOut:
-        mov [tmpStack], esp
-        xor ecx, ecx            ; ecx = 0
-        mov ebx, vec2           ; ebx = &vec
+mov [tmpStack], esp
+xor ecx, ecx ; ecx = 0
+mov ebx, vec2 ; ebx = &vec
 putVecLoop:
-        mov [tmp], ebx
-        cmp ecx, [vec_size]
-        je endOutputVector      ; to end of loop
-        mov [i], ecx
+mov [tmp], ebx
+cmp ecx, [vec_size]
+je endOutputVector ; to end of loop
+mov [i], ecx
 
-        ; output element
-        push dword [ebx]
-        push ecx
-        push strVecElemOut
-        call [printf]
+; output element
+push dword [ebx]
+push ecx
+push strVecElemOut
+call [printf]
 
-        mov ecx, [i]
-        inc ecx
-        mov ebx, [tmp]
-        add ebx, 4
-        jmp putVecLoop
+mov ecx, [i]
+inc ecx
+mov ebx, [tmp]
+add ebx, 4
+jmp putVecLoop
 endOutputVector:
-        mov esp, [tmpStack]
-        ret
-;--------------------------------------------------------------------------
+mov esp, [tmpStack]
+ret
+;------------------------------------------------------------------------—
 failedSize:
-        push vec_size
-        push strIncorSize
-        call [printf]
-        push 0
-        call [ExitProcess]
-newNeg:                      ;Сравнение с 0, если отрицательно 1
-        jl newNeg1
-        ret
-newNeg1:                     ;Отметка, что прошло отрицательное
-	mov [negv], 1
-	ret
+push vec_size
+push strIncorSize
+call [printf]
+push 0
+call [ExitProcess]
+newNeg: ;Сравнение с 0, если отрицательно 1
+jl newNeg1
+ret
+newNeg1: ;Отметка, что прошло отрицательное
+mov [negv], 1
+ret
 comparer:
-        je isnull
-        mov [edx], eax
-        ret
+je isnull
+mov [edx], eax
+ret
 isnull:
-	mov eni, [negv]
-	cmp eni, 1
-	call pushone
-	ret
+mov esi, [negv]
+cmp esi, 1
+call pushone
+ret
 pushone:
-	mov esi, 1
-        mov [edx], esi
-        ret
-;-------------------------------third act - including HeapApi--------------------------
-                                                 
+mov esi, 1
+mov [edx], esi
+ret
+;-------------------------------third act - including HeapApi------------------------—
+
 section '.idata' import data readable
-    library kernel, 'kernel32.dll',\
-            msvcrt, 'msvcrt.dll',\
-            user32,'USER32.DLL'
+library kernel, 'kernel32.dll',\
+msvcrt, 'msvcrt.dll',\
+user32,'USER32.DLL'
 
 include 'api\user32.inc'
 include 'api\kernel32.inc'
-    import kernel,\
-           ExitProcess, 'ExitProcess',\
-           HeapCreate,'HeapCreate',\
-           HeapAlloc,'HeapAlloc'
-  include 'api\kernel32.inc'
-    import msvcrt,\
-           printf, 'printf',\
-           scanf, 'scanf',\
-           getch, '_getch'
+import kernel,\
+ExitProcess, 'ExitProcess',\
+HeapCreate,'HeapCreate',\
+HeapAlloc,'HeapAlloc'
+include 'api\kernel32.inc'
+import msvcrt,\
+printf, 'printf',\
+scanf, 'scanf',\
+getch, '_getch'
